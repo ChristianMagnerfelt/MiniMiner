d6d732779e19c7bac6b00fc39b1a57eaa34819e0
Christian Magnerfelt <christian.magnerfelt@gmail.com>
2013-08-31 17:54:41 +0200

    Added sdl image library. Added imageToGLTexture and drawGLTexture functions to main.cpp.

--- source/main.cpp
+++ source/main.cpp
@@ -6,6 +6,7 @@
 ///
 #include <SDL.h>
 #include <SDL_opengl.h>
+#include <SDL_image.h>
 #include <gl/GL.h>
 #include <gl/GLU.h>
 
@@ -21,22 +22,31 @@ bool initializeSDL(
 	SDL_GLContext & context);
 bool initializeOpenGL(int32_t width, int32_t height);
 void render(SDL_Window * window);
+GLuint imageFileToGLTex(const char * fileName);
+void drawGLTexture(GLuint textureID, GLfloat x, GLfloat y, GLfloat width, GLfloat height);
 
 /// Main entry point
 int main( int argc, char* argv[] )
 {
+	Logger::LoggerManager::getInstance().init("debug.log");
+
 	const char * windowTitle = "MiniMiner";
 	SDL_Window * window;
 	SDL_GLContext context;
 	int32_t screenWidth = 800;
 	int32_t screenHeight = 600;
 	bool isRunning = true;
-
+	
 	if(!initializeSDL(windowTitle, screenWidth, screenHeight, window, context))
 		return 1;
 
 	if(!initializeOpenGL(screenWidth, screenHeight))
 		return 1;
+
+	int32_t flags=IMG_INIT_JPG|IMG_INIT_PNG;
+	IMG_Init(flags);
+
+	GLuint bgId = imageFileToGLTex("assets/BackGround.jpg");
 
 	// Enter main loop
 	SDL_Event event;
@@ -48,12 +58,15 @@ int main( int argc, char* argv[] )
 			case SDL_QUIT:
 				isRunning = false;
 		}
+		drawGLTexture(bgId, -1, -1, 2, 2);
 		render(window);
 	}
 
 	SDL_GL_DeleteContext(context);
 	SDL_DestroyWindow(window);
     SDL_Quit();
+
+	Logger::LoggerManager::getInstance().finalize();
 	return 0;
 }
 bool initializeSDL(
@@ -107,4 +120,45 @@ bool initializeOpenGL(int32_t width, int32_t height)
 void render(SDL_Window * window)
 {
 	SDL_GL_SwapWindow(window);
+}
+GLuint imageFileToGLTex(const char * fileName)
+{
+	GLuint id = 0;
+	auto surface = IMG_Load(fileName);
+	
+	if(surface == nullptr)
+	{
+		LOGGER_ERROR("%s", IMG_GetError());
+		LOGGER_ERROR("Could not load image : %s", fileName);
+		return 0;
+	}
+
+	glGenTextures(1, &id);
+	glBindTexture(GL_TEXTURE_2D, id);
+
+	GLint mode = GL_RGB;
+	if(surface->format->BytesPerPixel == 4)
+		mode = GL_RGBA;
+
+	glTexImage2D(GL_TEXTURE_2D, 0, mode, surface->w, surface->h, 0, mode, GL_UNSIGNED_BYTE, surface->pixels);
+ 
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+	SDL_FreeSurface(surface);
+	return id;
+}
+void drawGLTexture(GLuint textureID, GLfloat x, GLfloat y, GLfloat width, GLfloat height)
+{
+	glEnable(GL_TEXTURE_2D);
+	glBindTexture(GL_TEXTURE_2D, textureID);
+ 
+	glBegin(GL_QUADS);
+		glTexCoord2f(1.0f, 1.0f); glVertex3f(x, y, 0.0f);
+		glTexCoord2f(0.0f, 1.0f); glVertex3f(x + width, y, 0.0f);
+		glTexCoord2f(0.0f, 0.0f); glVertex3f(x + width, y + height, 0.0f);
+		glTexCoord2f(1.0f, 0.0f); glVertex3f(x, y + height, 0.0f);
+	glEnd();
+
+	glDisable(GL_TEXTURE);
 }
